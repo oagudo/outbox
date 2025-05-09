5a4c25c8e78666b8130d127e18eff7ef357194b1
omaragudo <omar.agudo@gmail.com>
2025-05-09 10:29:25 +0200

    extract test setup

---
+++ test/setup_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/outbox?sslmode=disable")
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %s", err)
+	}
+
+	log.Println("Connected to database")
+	_, err = db.Exec("TRUNCATE TABLE Outbox")
+	if err != nil {
+		log.Fatalf("Failed to truncate outbox table: %s", err)
+	}
+
+	_, err = db.Exec("TRUNCATE TABLE Entity")
+	if err != nil {
+		log.Fatalf("Failed to truncate entity table: %s", err)
+	}
+
+	os.Exit(m.Run())
+}
--- test/writer_test.go
+++ test/writer_test.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"log"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/lib/pq"
-	_ "github.com/lib/pq"
 	"github.com/oagudo/outbox/pkg/outbox"
 	"github.com/stretchr/testify/require"
 )
@@ -23,27 +20,6 @@ var (
 type entity struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
-}
-
-func TestMain(m *testing.M) {
-	var err error
-	db, err = sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/outbox?sslmode=disable")
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %s", err)
-	}
-
-	log.Println("Connected to database")
-	_, err = db.Exec("TRUNCATE TABLE Outbox")
-	if err != nil {
-		log.Fatalf("Failed to truncate outbox table: %s", err)
-	}
-
-	_, err = db.Exec("TRUNCATE TABLE Entity")
-	if err != nil {
-		log.Fatalf("Failed to truncate entity table: %s", err)
-	}
-
-	os.Exit(m.Run())
 }
 
 func TestWriterSuccessfullyWritesToOutbox(t *testing.T) {
