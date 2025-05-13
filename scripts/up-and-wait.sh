f41e966c55cd451662babd70aaf772a7ea823af9
omaragudo <omar.agudo@gmail.com>
2025-05-13 11:42:40 +0200

    use docker command for compose

--- scripts/up-and-wait.sh
+++ scripts/up-and-wait.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Run in daemon mode
-docker-compose up -d
+docker compose up -d
 
 echo "Waiting for containers to become healthy..."
 
 # Wait for all containers to be healthy
 while true; do
-    unhealthy=$(docker inspect --format='{{.Name}} {{.State.Health.Status}}' $(docker-compose ps -q) | grep -v healthy || true)
+    unhealthy=$(docker inspect --format='{{.Name}} {{.State.Health.Status}}' $(docker compose ps -q) | grep -v healthy || true)
     if [ -z "$unhealthy" ]; then
         echo "All containers are healthy."
         break
